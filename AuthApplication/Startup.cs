b783545b29bf4e08a34fd9110613fcfbf3af9617
y.ustinovich <y.ustinovich@a1qa.com>
2019-02-04 16:26:38 +0300

    first

---
+++ AuthApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AuthApplication.Startup))]
+namespace AuthApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
