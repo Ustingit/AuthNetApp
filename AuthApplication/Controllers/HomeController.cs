5a7e8ef5a0c9ff87d5a01453124df3c5e851fc9f
Ustingit <ustinx@mail.ru>
2019-02-04 17:52:25 +0300

    12.14 - https://metanit.com/sharp/mvc5/12.14.php --------------- AspNetRoles: содержит определения ролей
    AspNetUserClaims: таблица, хранящая набор клеймов (claim). Claim представляет иную модель авторизации по сравнению с ролями. Грубо говоря, claim содержит некоторую информацию о пользователе, например, адрес электронной почты, логин, возраст и т.д. И эта информация позволяет идентифицировать пользователя и наделить его соответствующими правами доступа.
    
    AspNetUserLogins: таблица логинов пользователя
    
    AspNetUserRoles: таблица, устанавливающая для пользователей определенные роли
    
    AspNetUsers: собственно таблица пользователей. Если мы ее откроем, то увидим данные зарегистрированного пользователя
    ---------------------
    В приложении мы не работаем напрямую с классами IdentityUser и IdentityDbContext, а имеем дело с классами-наследниками.
    -----------------------
    Файл IdentityConfig.cs содержит ряд дополнительных вспомогательных классов: сервисы для двухфакторной валидации с помощью email и телефона EmailService и SmsService, класс менеджера пользователей ApplicationUserManager, добавляющий к UserManager ряд дополнительных функций, и класс ApplicationSignInManager, используемый для входа и выхода с сайта.
    ------------
    IdentityUser реализует интерфейс IUser и определяет следующие свойства:
    
    Claims: возвращает коллекцию claims - специальных атрибутов, которыми обладает пользователь и которые хранят о пользователе определенную информацию
    
    Email: email пользователя
    
    Id: уникальный идентификатор пользователя
    
    Logins: возвращает коллекцию логинов пользователя
    
    PasswordHash: возвращает хэш пароля
    
    Roles: возвращает коллекцию ролей пользователя
    
    PhoneNumber: возвращает номер телефона
    
    SecurityStamp: возвращает некоторое значение, которое меняется при каждой смене настроек аутентификации для данного пользователя
    
    UserName: возвращает ник пользователя
    
    AccessFailedCount: число попыток неудачного входа в систему
    
    EmailConfirmed: возвращает true, если email был подтвержден
    
    PhoneNumberConfirmed: возвращает true, если телефонный номер был подтвержден
    
    TwoFactorEnabled: если равен true, то для данного пользователя включена двухфакторная авторизация

--- AuthApplication/Controllers/HomeController.cs
+++ AuthApplication/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AuthApplication.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,14 @@ namespace AuthApplication.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Users()
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                return View(db.Users.ToList());
+            }
         }
 
         public ActionResult About()
