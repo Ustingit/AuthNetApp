89e9737f21573a763301590315267d7ee36fdc1a
Ustingit <ustinx@mail.ru>
2019-02-06 18:22:07 +0300

    12.3 full working

--- AuthApplication/Controllers/AccountController.cs
+++ AuthApplication/Controllers/AccountController.cs
@@ -151,7 +151,7 @@ namespace AuthApplication.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, Age = model.Age };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
--- AuthApplication/Models/IdentityModels.cs
+++ AuthApplication/Models/IdentityModels.cs
@@ -9,6 +9,8 @@ namespace AuthApplication.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public int Age { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -18,8 +20,17 @@ namespace AuthApplication.Models
         }
     }
 
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+    }
+
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Book> Books { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
@@ -31,3 +42,4 @@ namespace AuthApplication.Models
         }
     }
 }
+
